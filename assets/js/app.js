2e76a796cc1d7f0a5c76777dd7c263f97a8775c0
Wenhao Wu <wenhaow@hotmail.com>
2019-10-19 19:51:28 -0700

    can't display on GitHub

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,14 @@
 function forDelayDisplay() {
 $('#cirHtml').css('display', 'none');
-$('#cirHtml').delay(250).fadeIn(500);
+$('#cirHtml').delay(500).fadeIn(1000);
 $('#cirCss').css('display', 'none');
-$('#cirCss').delay(750).fadeIn(1000);
+$('#cirCss').delay(1000).fadeIn(1500);
 $('#cirJava').css('display', 'none');
-$('#cirJava').delay(1250).fadeIn(1500);
+$('#cirJava').delay(1500).fadeIn(2000);
 $('#cirNodeJs').css('display', 'none');
-$('#cirNodeJs').delay(1750).fadeIn(2000);
+$('#cirNodeJs').delay(2500).fadeIn(3000);
 $('#cirFullStack').css('display', 'none');
-$('#cirFullStack').delay(2250).fadeIn(2500);
-$('#cirFullStack').delay(2750).fadeOut(3000);
-$('#cirFullStack').css('display', 'none');
-$('#cirNodeJs').delay(3250).fadeOut(3500);
-$('#cirNodeJs').css('display', 'none');
-$('#cirJava').delay(3750).fadeOut(4000);
-$('#cirJava').css('display', 'none');
-$('#cirCss').delay(4250).fadeOut(4500);
-$('#cirCss').css('display', 'none');
-$('#cirHtml').delay(4750).fadeOut(5000);
-$('#cirHtml').css('display', 'none');
+$('#cirFullStack').delay(3500).fadeIn(4000);
 }
 
-forDelayDisplay();
-forDelayDisplay();
-forDelayDisplay();
-forDelayDisplay();
-forDelayDisplay();
 forDelayDisplay();
