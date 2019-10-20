37e4808d8846808aa9d82b41bf13127094df6e81
Wenhao Wu <wenhaow@hotmail.com>
2019-10-19 20:47:16 -0700

    Another try

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,9 +11,9 @@ $('#cirFullStack').css('display', 'none');
 $('#cirFullStack').delay(3500).fadeIn(4000);
 }
 
-forDelayDisplay();
 
-var makeItRain = function() {
+
+function makeItRain() {
     //clear out everything
     $('.rain').empty();
   
@@ -37,23 +37,6 @@ var makeItRain = function() {
     $('.rain.front-row').append(drops);
     $('.rain.back-row').append(backDrops);
   }
-  
-  // $('.splat-toggle.toggle').on('click', function() {
-  //   $('body').toggleClass('splat-toggle');
-  //   $('.splat-toggle.toggle').toggleClass('active');
-  //   makeItRain();
-  // });
-  
-  // $('.back-row-toggle.toggle').on('click', function() {
-  //   $('body').toggleClass('back-row-toggle');
-  //   $('.back-row-toggle.toggle').toggleClass('active');
-  //   makeItRain();
-  // });
-  
-  // $('.single-toggle.toggle').on('click', function() {
-  //   $('body').toggleClass('single-toggle');
-  //   $('.single-toggle.toggle').toggleClass('active');
-  //   makeItRain();
-  // });
-  
+ 
   makeItRain();
+  forDelayDisplay();
