603072543933b8fa8f0f0544f42198d0aae70db0
Wenhao Wu <wenhaow@hotmail.com>
2019-10-19 18:10:28 -0700

    Adding animated Skills page

---
+++ assets/js/app - old.js
@@ -0,0 +1,8 @@
+$('#myImg').css('display', 'none');
+$('#myImg').delay(500).fadeIn(1000);
+$('#cirResume').css('display', 'none');
+$('#cirResume').delay(1000).fadeIn(1500);
+$('#cirProject').css('display', 'none');
+$('#cirProject').delay(1500).fadeIn(2000);
+$('#cirSkill').css('display', 'none');
+$('#cirSkill').delay(2000).fadeIn(2500);
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,29 @@
-$('#myImg').css('display', 'none');
-$('#myImg').delay(500).fadeIn(1000);
-$('#cirResume').css('display', 'none');
-$('#cirResume').delay(1000).fadeIn(1500);
-$('#cirProject').css('display', 'none');
-$('#cirProject').delay(1500).fadeIn(2000);
-$('#cirSkill').css('display', 'none');
-$('#cirSkill').delay(2000).fadeIn(2500);
+function forDelayDisplay() {
+$('#cirHtml').css('display', 'none');
+$('#cirHtml').delay(250).fadeIn(500);
+$('#cirCss').css('display', 'none');
+$('#cirCss').delay(750).fadeIn(1000);
+$('#cirJava').css('display', 'none');
+$('#cirJava').delay(1250).fadeIn(1500);
+$('#cirNodeJs').css('display', 'none');
+$('#cirNodeJs').delay(1750).fadeIn(2000);
+$('#cirFullStack').css('display', 'none');
+$('#cirFullStack').delay(2250).fadeIn(2500);
+$('#cirFullStack').delay(2750).fadeOut(3000);
+$('#cirFullStack').css('display', 'none');
+$('#cirNodeJs').delay(3250).fadeOut(3500);
+$('#cirNodeJs').css('display', 'none');
+$('#cirJava').delay(3750).fadeOut(4000);
+$('#cirJava').css('display', 'none');
+$('#cirCss').delay(4250).fadeOut(4500);
+$('#cirCss').css('display', 'none');
+$('#cirHtml').delay(4750).fadeOut(5000);
+$('#cirHtml').css('display', 'none');
+}
+
+forDelayDisplay();
+forDelayDisplay();
+forDelayDisplay();
+forDelayDisplay();
+forDelayDisplay();
+forDelayDisplay();
